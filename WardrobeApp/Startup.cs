8b78b9ad633529ef78fa2406c62134a0d362335c
jkirouac42 <jkirouac42@gmail.com>
2016-07-21 10:36:22 -0400

    initial commit

---
+++ WardrobeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WardrobeApp.Startup))]
+namespace WardrobeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
